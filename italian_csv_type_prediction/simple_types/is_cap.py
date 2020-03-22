6a7a772d93295c3ea2e1e8755668cf589f07e34a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-22 23:17:27 +0100

    Fixed CAP heuristic

--- italian_csv_type_prediction/simple_types/is_cap.py
+++ italian_csv_type_prediction/simple_types/is_cap.py
@@ -6,5 +6,9 @@ cap_regex = re.compile(r"^\d{5}$")
 def is_cap(candidate)->bool:
     """Return a boolean representing if given candidate is a valid italian CAP."""
     if isinstance(candidate, (str, int, float)):
-        return bool(cap_regex.match(str(candidate).zfill(5))) and not is_nan(candidate)
+        try:
+            return bool(cap_regex.match(str(int(candidate)).zfill(5))) and not is_nan(candidate)
+        except ValueError:
+            return False
+        
     return False
--- tests/test_is_cap.py
+++ tests/test_is_cap.py
@@ -3,4 +3,4 @@ from .utils import default_test
 
 
 def test_is_cap():
-    default_test(is_cap, ["CAP"], black_list=["Year", "Integer", "HouseNumber"])
+    default_test(is_cap, ["CAP"], black_list=["Year", "Integer", "Float", "HouseNumber"])
