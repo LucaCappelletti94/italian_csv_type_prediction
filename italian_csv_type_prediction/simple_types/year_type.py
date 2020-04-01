f9d64c1fd6ed1dc4e753183ce108f11cd1916608
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-04-01 17:33:06 +0200

    Fixed error in parsing years

--- italian_csv_type_prediction/simple_types/year_type.py
+++ italian_csv_type_prediction/simple_types/year_type.py
@@ -6,7 +6,7 @@ class YearType(IntegerType):
     def validate(self, candidate, **kwargs) -> bool:
         """Return boolean representing if given candidate can be considered a Year."""
         if super().validate(candidate, **kwargs):
-            candidate = int(float(candidate))
+            candidate = int(float(str(candidate).replace(",", ".")))
             return any(
                 candidate > _max and candidate < _min
                 for _max, _min in ((1990, 2030),)
