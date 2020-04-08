212d8fd0a3ffc2381cc4b2f6bdd534d8d2c1227f
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-04-08 17:37:13 +0200

    Identified new corner case

--- italian_csv_type_prediction/simple_types/integer_type.py
+++ italian_csv_type_prediction/simple_types/integer_type.py
@@ -9,4 +9,4 @@ class IntegerType(FloatType):
 
     def validate(self, candidate, **kwargs) -> bool:
         """Return boolean representing if given candidate can be considered integer."""
-        return super().validate(candidate, **kwargs) and float(str(candidate).replace(",", ".")).is_integer()
+        return super().validate(candidate, **kwargs) and super().convert(candidate).is_integer()
