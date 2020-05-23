e106eba745477f3ddbf9ada2e1e2c701b85a7324
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-05-23 21:36:00 +0200

    Captured output

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-git clone https://github.com/openvenues/libpostal
+git clone https://github.com/openvenues/libpostal  > /dev/null 2>&1
 cd libpostal
-./bootstrap.sh
-./configure
-make
-sudo make install
+./bootstrap.sh > /dev/null 2>&1
+./configure > /dev/null 2>&1
+make > /dev/null 2>&1
+sudo make install > /dev/null 2>&1
 cd ..
 rm -fdr libpostal
