21db873ed9ebb7e373695ae64463b6d65b8b8fac
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-05-24 17:25:33 +0200

    Extended testing

--- tests/test_namesurname.py
+++ tests/test_namesurname.py
@@ -6,19 +6,28 @@ def test_namesurname():
     name_surname_predictor = NameSurnameType()
     surname_name_predictor = SurnameNameType()
 
-    name = "Maria Teresa"
-    surname = "Lupi"
-    fiscal_code = codicefiscale.encode(
-        surname=surname, name=name, sex='M', birthdate='01/01/1990', birthplace='Roma')
+    candidates = [
+        ("Maria Teresa", "Lupi", None),
+        ("NUNZIO", "SOFIA", "SFONNZ56T16A093W"),
+        ("RINA", "BRUTTOMESSO", "BRTRNI67E57E349J"),
+        ("LINO", "ANZOLIN", "NZLLNI74R09F964X"),
+        ("MARIO", "CRISCUOLO", "CRSMRA70S13C129M"),
+        ("GINO", "DONADELLO", "DNDGNI73E27E682L"),
+    ]
 
-    name_surname = f"{name} {surname}"
-    surname_name = f"{surname} {name}"
+    for name, surname, fiscal_code in candidates:
+        if fiscal_code is None:
+            fiscal_code = codicefiscale.encode(
+                surname=surname, name=name, sex='M', birthdate='01/01/1990', birthplace='Roma')
 
-    assert name_surname_predictor.validate(
-        name_surname, fiscal_code=fiscal_code)
-    assert surname_name_predictor.validate(
-        surname_name, fiscal_code=fiscal_code)
-    assert not name_surname_predictor.validate(
-        surname_name, fiscal_code=fiscal_code)
-    assert not surname_name_predictor.validate(
-        name_surname, fiscal_code=fiscal_code)
+        name_surname = f"{name} {surname}"
+        surname_name = f"{surname} {name}"
+
+        assert name_surname_predictor.validate(
+            name_surname, fiscal_code=fiscal_code)
+        assert surname_name_predictor.validate(
+            surname_name, fiscal_code=fiscal_code)
+        assert not name_surname_predictor.validate(
+            surname_name, fiscal_code=fiscal_code)
+        assert not surname_name_predictor.validate(
+            name_surname, fiscal_code=fiscal_code)
