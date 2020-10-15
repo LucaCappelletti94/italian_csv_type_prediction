9e62510253e4460fce2e50d078bbcc5d85df5ba0
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-10-15 22:01:22 +0200

    Added proper exception when translation fails

--- italian_csv_type_prediction/utils/translate_types.py
+++ italian_csv_type_prediction/utils/translate_types.py
@@ -16,21 +16,63 @@ class TranslateType:
         }
 
     def translate(self, value_type: Union[List, Tuple]) -> str:
-        """Return value type translated to given language."""
+        """Return value type translated to given language.
+        
+        Parameters
+        ----------------------
+        value_type: Union[str, List, Tuple],
+            Either the value to be translated or a list of values.
+
+        Raises
+        ----------------------
+        ValueError,
+            If the value is not available in the dictionary.
+
+        Returns
+        ----------------------
+        The translated value.
+        """
         if isinstance(value_type, (list, tuple)):
             return tuple([
                 self.translate(v)
                 for v in value_type
             ])
+        if value_type not in self._translations:
+            raise ValueError(
+                "The given value {} is not available in the dictionary.".format(
+                    value_type
+                )
+            )
         return self._translations[value_type]
 
-    def reverse_translate(self, value_type: Union[List, Tuple]) -> str:
-        """Return value type translated back to english from given language."""
+    def reverse_translate(self, value_type: Union[str, List, Tuple]) -> str:
+        """Return value type translated back to english from given language.
+        
+        Parameters
+        ----------------------
+        value_type: Union[str, List, Tuple],
+            Either the value to be back translated or a list of values.
+
+        Raises
+        ----------------------
+        ValueError,
+            If the value is not available in the dictionary.
+
+        Returns
+        ----------------------
+        The back translated value.
+        """
         if isinstance(value_type, (list, tuple)):
             return tuple([
                 self._reverse_translations(v)
                 for v in value_type
             ])
+        if value_type not in self._reverse_translations:
+            raise ValueError(
+                "The given value '{}' is not available in the dictionary.".format(
+                    value_type
+                )
+            )
         return self._reverse_translations[value_type]
 
     def translate_dataframe(self, df: pd.DataFrame) -> pd.DataFrame:
