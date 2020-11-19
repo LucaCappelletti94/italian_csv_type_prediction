bee7a2591b76698c2153511cf63cccc3f2103b19
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-11-19 20:47:34 +0100

    Introduced translator object

--- italian_csv_type_prediction/utils/translator.py
+++ italian_csv_type_prediction/utils/translator.py
@@ -1,22 +1,19 @@
 import compress_json
 import pandas as pd
-import os
 from typing import Tuple, List, Union
-from .translator import Translator
 
 
-class TranslateType:
+class Translator:
 
-    SUPPORTED_LANGUAGES = ("it", )
+    ERROR_HANDLING = ("raise", "pass")
 
-    def __init__(self, language: str = "it", error_handling: str = "raise"):
+    def __init__(self, path: str, error_handling: str = "raise"):
         """Create new object to translate types to given language.
 
         Parameters
         ----------------------
-        language: str = "it",
-            The language to translate the strings to.
-            Currently supporting only italian language.
+        path: str,
+            Path from where to load the dictionary.
         error_handling: str = "raise",
             Behaviour of the translator when a given string is not available.
             The possible behaviours are 'raise' or 'pass'.
@@ -29,25 +26,12 @@ class TranslateType:
         ValueError,
             When the given error handling is not supported.
         """
-        if language not in TranslateType.SUPPORTED_LANGUAGES:
-            raise ValueError(
-                (
-                    "Given language '{language}' is not supported. "
-                    "The supported languages are {supported_languages}."
-                ).format(
-                    language=language,
-                    supported_languages=", ".join(
-                        TranslateType.SUPPORTED_LANGUAGES
-                    )
-                )
-            )
-        self._translator = Translator(
-            path=os.path.join(
-                os.path.dirname(__file__),
-                "{}.json".format(language)
-            ),
-            error_handling=error_handling
-        )
+        self._translations = compress_json.load(path)
+        self._error_handling = error_handling
+        self._reverse_translations = {
+            v: k
+            for k, v in self._translations.items()
+        }
 
     def translate(self, value_type: Union[List, Tuple]) -> str:
         """Return value type translated to given language.
@@ -66,7 +50,21 @@ class TranslateType:
         ----------------------
         The translated value.
         """
-        return self._translator.translate(value_type)
+        if isinstance(value_type, (list, tuple)):
+            return tuple([
+                self.translate(v)
+                for v in value_type
+            ])
+        if value_type not in self._translations:
+            if self._error_handling == "raise":
+                raise ValueError(
+                    "The given value {} is not available in the dictionary.".format(
+                        value_type
+                    )
+                )
+            if self._error_handling == "pass":
+                return value_type
+        return self._translations[value_type]
 
     def reverse_translate(self, value_type: Union[str, List, Tuple]) -> str:
         """Return value type translated back to english from given language.
@@ -85,8 +83,22 @@ class TranslateType:
         ----------------------
         The back translated value.
         """
-        return self._translator.reverse_translate(value_type)
+        if isinstance(value_type, (list, tuple)):
+            return tuple([
+                self._reverse_translations(v)
+                for v in value_type
+            ])
+        if value_type not in self._reverse_translations:
+            if self._error_handling == "raise":
+                raise ValueError(
+                    "The given value {} is not available in the dictionary.".format(
+                        value_type
+                    )
+                )
+            if self._error_handling == "pass":
+                return value_type
+        return self._reverse_translations[value_type]
 
     def translate_dataframe(self, df: pd.DataFrame) -> pd.DataFrame:
         """Translate all dataframe."""
-        return self._translator.translate_dataframe(df)
+        return df.applymap(self.translate)
